105e930b31c3ec9644216e97b26243e40cf8ac6a
Tulio Faria <tuliofaria@gmail.com>
2024-05-30 13:58:26 -0300

    run it

--- run.js
+++ run.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const shell = require("shelljs");
 const axios = require("axios");
 
-const DISCORD_WEBHOOK = process.env.DISCORD_WEBHOOK;
+const DISCORD_WEBHOOK =
+  "https://discord.com/api/webhooks/1245780885744128062/D4FZu5hQjQinrcGfG6mfYp7xQ06Gl3_AsxBOcOzEs7-eDSkKp4hDDzylou8tYhj60XsC";
 
 async function run() {
   const output = await shell.exec("pnpm exec playwright test");
